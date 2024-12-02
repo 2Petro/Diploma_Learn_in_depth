ede8eca3e4fe4864d6f968511dbd91444100101b
2Petro <petronazehokok@gmail.com>
2024-12-02 02:16:11 +0200

    Add progress state

--- create_new_study.sh
+++ create_new_study.sh
@@ -14,5 +14,14 @@ touch "_UNIT_$1/Lesson_$2/$3"
 
 #Update progress
 
-echo "| $1/$2 | $3 | ❓ In progress | " >> README.md
+if [ "$4" -eq 1 ]; then
+  status_text="✅ Completed"
+elif [ "$4" -eq 0 ]; then
+  status_text="❓ In Progress"
+else
+echo "Error: Invalid status. Please provide 0 (Completed) or 1 (In Progress)."
+  exit 1
+fi
+
+echo "| $1/$2 | $3 | $status_text | " >> README.md
 
