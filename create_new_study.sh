c1a31d88b9e8263ee0f8a4642b2d070d5ce9c755
2Petro <petronazehokok@gmail.com>
2024-12-02 02:03:57 +0200

    Fix some issues

--- create_new_study.sh
+++ create_new_study.sh
@@ -3,13 +3,14 @@
 #UNIT requires
 
 mkdir -p "_UNIT_$1/Lesson_$2"
-touch "_UNIT_$1/Lesson_$2/README.md"
-echo -e "Status / \n\n- **Unit $1:** ❓ In progress" > "_UNIT_$1/Lesson_$2/README.md"
+echo -e "Status / \n\n- **Unit $1:** $3 ❓ In progress" > "_UNIT_$1/Lesson_$2/README.md"
 
 #Lesson requires
 
 touch "_UNIT_$1/Lesson_$2/notes"
 touch "_UNIT_$1/Lesson_$2/main.c"
+touch "_UNIT_$1/Lesson_$2/README.md"
+touch "_UNIT_$1/Lesson_$2/$3"
 
 #Update progress
 
